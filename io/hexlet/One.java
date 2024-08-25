6c3c3b1b417ff0d65719826eca080223fa0988a9
killdom2 <kldm-2@mail.ru>
2024-08-25 17:50:08 +0400

    add org.apache.commons.lang3 and some updates One.java, but it's not working yet

--- io/hexlet/One.java
+++ io/hexlet/One.java
@@ -1,8 +1,13 @@
 package io.hexlet;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class One {
 	public static void main(String[] args) {
 		Two.greeting();
+		var str = "Hexlet";
+		var rev = StringUtils.reverse(str);
+		System.out.println(rev);
 	}
 }
 
